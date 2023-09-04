0b8511fb137d3fc7ed44b97c20cf85be7f39bdec
RAJKUMAR <103421948+rajkumar2255n@users.noreply.github.com>
2023-09-05 01:54:53 +0530

    end of the whole code

--- practicethreejs/main.js
+++ practicethreejs/main.js
@@ -1,44 +1,107 @@
-import "./style.css"
-import * as Three from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import "./style.css";
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 const scene = new Three.Scene();
-const camera = new Three.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+const camera = new Three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new Three.WebGLRenderer({
- canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth,window.innerHeight)
-camera.position.setZ(30);
-renderer.render(scene,camera);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(20);
 
-const geometry = new Three.TorusGeometry(10,3,16,100)
-// const material = new Three.MeshBasicMaterial({color:0xFF6347,wireframe:true})
-const material = new Three.MeshStandardMaterial({color:0xFF6347})
-const torus = new Three.Mesh(geometry,material);
-scene.add(torus)
-const pointLight = new Three.PointLight(0xFFFFFF)
-pointLight.position.set(5,5,5)
+const geometry = new Three.TorusGeometry(10, 3, 16, 100);
+const material = new Three.MeshStandardMaterial({
+  color: 0xFF6347,
+  wireframe: true,
+});
+const torus = new Three.Mesh(geometry, material);
+scene.add(torus);
 
-const ambitLight = new Three.AmbientLight(0xffffff);
-scene.add(pointLight,ambitLight)
+const pointLight = new Three.PointLight(0xFFFFFF);
+pointLight.position.set(5, 5, 5);
 
-const lightHelper = new Three.PointLightHelper(pointLight)
-const gridHelper = new Three.GridHelper(200,50);
+const ambientLight = new Three.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
 
-scene.add(lightHelper,gridHelper)
+function addStar() {
+  const geometry = new Three.SphereGeometry(0.25, 24, 24);
+  const material = new Three.MeshStandardMaterial({ color: 0xffffff });
+  const star = new Three.Mesh(geometry, material);
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => Three.MathUtils.randFloatSpread(100));
 
-const controls = new OrbitControls(camera,renderer.domElement)
+  star.position.set(x, y, z);
+  scene.add(star);
+}
 
-scene.add(pointLight)
-function animate(){
+Array(200).fill().forEach(addStar);
+
+scene.add(pointLight);
+
+const spaceTexture = new Three.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+const rajTexture = new Three.TextureLoader().load("raj.jpg");
+
+const raj = new Three.Mesh(
+  new Three.BoxGeometry(3, 3, 3),
+  new Three.MeshBasicMaterial({ map: rajTexture })
+);
+
+scene.add(raj);
+
+const moonTexture = new Three.TextureLoader().load("moon.jpg");
+const normalTexture = new Three.TextureLoader().load("normal.jpg");
+const moon = new Three.Mesh(
+  new Three.SphereGeometry(3, 32, 32),
+  new Three.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  raj.rotation.y += 0.01;
+  raj.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
   requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
-  controls.update()
-  renderer.render(scene,camera);
 
+  // Ensure to update the controls
+  controls.update();
+
+  renderer.render(scene, camera);
 }
-animate()
 
+animate();
